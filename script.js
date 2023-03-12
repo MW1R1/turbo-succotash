e86ef7fcb208d973add902885f788b54f7a7163a
MW1R1 <nickmwirigi58@gmail.com>
2023-03-12 20:32:50 +0300

    further customization

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+// code to handle form submission
+const form = document.querySelector("form");
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+event.preventDefault();
+const email = event.target.querySelector("input[type=email]").value;
+if (email !== "") {
+alert(`Thank you for signing up with email: ${email}`);
+event.target.reset();
+} else {
+alert("Please enter a valid email address.");
+}
+}
